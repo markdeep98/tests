f927a738c8c23ef45ee69d8a7fb17fe19748523f
markdeep98 <dmitriy.gladky98@gmail.com>
2018-06-08 07:50:51 -0700

    .

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,15 +1,31 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
+	def setup
+		@article = Article.new(title: "New article")
+	end
+
   test "should not save article without title" do
-  	article = Article.new
-  	assert_not article.save
+  	assert @article.save
   end
 
-  test "should report error" do
-  	assert_raises(NameError) do
-  		some_var
-  	end
+  test "should be valid" do
+  	assert @article.valid?
   end
 
+  test "title should be present" do
+  	@article.title = ""
+  	assert_not @article.valid?
+  end
+
+  test "title should not be too long" do
+  	@article.title = "a" * 51
+  	assert_not @article.valid?
+  end
+
+  test "title should be unique" do
+  	duplicate_title = @article.dup
+  	@article.save
+  	assert_not duplicate_title.valid?
+  end
 end
